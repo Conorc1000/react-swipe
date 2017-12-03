67796ea1f55d9f28d5747e10659327b710a9fcef
Conor Campbell <conorc1000@gmail.com>
2017-12-03 19:04:55 +0000

    adds utils and waiterData.

---
+++ src/waiterData.js
@@ -0,0 +1,9 @@
+const data = [
+    {name :'Joe', pictureUrl: 'https://www.collinsdictionary.com/images/thumb/waiter_110620211_250.jpg'},
+    {name :'Geoff', pictureUrl: 'http://www.telegraph.co.uk/content/dam/women/2016/10/13/110830408WAITER-WOMEN_trans_NvBQzQNjv4BqEFUnkns9FD4ra5dySZvT8LeJq8AVX9mzGm4FY-Kw_IM.jpg'},
+    {name :'Sally',  pictureUrl:'http://thekindtips.com/wp-content/uploads/How-to-Make-a-Budget-as-a-Waitress.jpg'},
+    {name :'Rob',  pictureUrl: 'https://az616578.vo.msecnd.net/files/2016/07/18/636044020543722165-1925093937_waiter_tray_white_background_79870_2048x1152.jpg'},
+    {name :'Jane',  pictureUrl: 'http://media.gettyimages.com/photos/waitress-standing-with-two-cups-of-coffee-on-tray-in-cafe-portrait-picture-idsb10061169c-001'}
+];
+
+export default data;
